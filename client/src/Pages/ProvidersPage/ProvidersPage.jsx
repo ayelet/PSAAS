8187625afa1856d29ac15c2cbd30c7b18109e948
Ayelet <ayeletd@gmail.com>
2021-05-20 03:37:10 +0300

    refactors search filters

--- client/src/Pages/ProvidersPage/ProvidersPage.jsx
+++ client/src/Pages/ProvidersPage/ProvidersPage.jsx
@@ -1,20 +1,35 @@
 import "./ProvidersPage.css";
 
 import React from "react";
-import Filter from "../../components/Filter/Filter";
+import Filter from "../../components/Filters/Filter/Filter";
 import ProviderCard from "../../components/ProviderCard/ProviderCard";
-import FilterRating from "../../components/FilterRating/FilterRating";
-import FilterByDate from "../../components/FilterByDate/FilterByDate";
+import FilterRating from "../../components/Filters/FilterRating/FilterRating";
+import FilterByDate from "../../components/Filters/FilterByDate/FilterByDate";
 import FilterProviderType from "../../components/Filters/FilterProviderType/FilterProviderType";
+import FilterPrice from "../../components/Filters/FilterPrice/FilterPrice";
 // import { Card } from "react-bootstrap";
 
 export const ProvidersPage = () => {
-  //  handleCallback = (childData) => {
-  //    this.setState({ data: childData });
-  //  };
+
 
   const handleFilterByRating = (data) => {
     console.log("Parent: option chosen: ", data);
+  };
+
+  const handleFilterByServiceType = (data) => {
+    console.log("Parent: service types: ", data);
+  };
+
+  const handleFilterByDate = (data) => {
+    console.log("Parent: dates:, ", data);
+  };
+
+  const handleFilterByLocation = (data) => {
+    console.log("Parent: filter by distance: ", data);
+  };
+
+  const handleFilterByPrice = (data) => {
+    console.log("Parent callback = price filter: ", data);
   };
 
   return (
@@ -24,15 +39,15 @@ export const ProvidersPage = () => {
           <FilterRating parentCallback={handleFilterByRating} />
         </div>
         <div className="filter-wrapper">
-          <FilterByDate />
+          <FilterByDate parentCallback={handleFilterByDate} />
         </div>
         <div className="filter-wrapper">
-          <FilterProviderType />
+          <FilterProviderType parentCallBack={handleFilterByServiceType} />
         </div>
-
-        <Filter name="Search By Price" />
-        <Filter name="Filter 2" />
-        <Filter name="Filter 3" />
+        <div className="filter-wrapper">
+          <FilterPrice parentCallBack={handleFilterByPrice} />
+        </div>
+        
       </aside>
       <main className="providers-list">
         <ProviderCard />
