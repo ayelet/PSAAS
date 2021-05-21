3dbb651806ea72ed0153ad427640a0f5ff80f6c2
Ayelet <ayeletd@gmail.com>
2021-05-21 04:04:12 +0300

    changes name of reducers library (lowercase)

--- client/src/helpers/store.js
+++ client/src/helpers/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import thunkMiddleware from "redux-thunk";
 import { createLogger } from "redux-logger";
-import rootReducer from "../reducers1";
+import rootReducer from "../reducers";
 
 const loggerMiddleware = createLogger();
 
