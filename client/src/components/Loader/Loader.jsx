e83670ceefe7d3c009ef6762651e25a04d5997c3
Ayelet <ayeletd@gmail.com>
2021-05-23 04:48:04 +0300

    adds loader component

---
+++ client/src/components/Loader/Loader.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import rollingCat from "../../assets/img/rolling cat.gif";
+
+const Loader = () => {
+  return (
+    <Container className="loader" center>
+      <img src={rollingCat} alt="rolling cat" />
+
+      <h3>Loading...</h3>
+    </Container>
+  );
+};
+
+export default Loader;
