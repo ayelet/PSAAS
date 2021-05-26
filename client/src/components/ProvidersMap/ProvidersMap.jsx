7c4d47b503b7e855ae86106b104be8bd1196793c
Ayelet <ayeletd@gmail.com>
2021-05-26 17:00:52 +0300

    changes to google-map-react

--- client/src/components/ProvidersMap/ProvidersMap.jsx
+++ client/src/components/ProvidersMap/ProvidersMap.jsx
@@ -1,28 +1,36 @@
-import React from "react";
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker,
-} from "react-google-maps";
+// import { GoogleMap, Marker } from "react-google-maps";
+import React, { Component } from "react";
+import GoogleMapReact from "google-map-react";
+import "./ProvidersMap.css";
 
-const ProvidersMap = withScriptjs(
-  withGoogleMap((props) => (
-    <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
-      {props.isMarkerShown && (
-        <Marker position={{ lat: -34.397, lng: 150.644 }} />
-      )}
-    </GoogleMap>
-  ))
-);
+// const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const MyMarker = () => <div className="map-marker"></div>;
+
+const GOOGLE_MAP_KEY = "AIzaSyAUQ730RK1Rsk0WPS97t659_4VN1S7GN68";
+class ProvidersMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 32.794044,
+      lng: 34.989571,
+    },
+    zoom: 12,
+  };
+
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: "80vh", width: "100%" }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: GOOGLE_MAP_KEY }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          {/*  <Marker lat={this.props.center.lat} lng={this.props.center.lng} /> */}
+          <MyMarker lat={32.794044} lng={34.989571} />
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
 
 export default ProvidersMap;
-
-// <MyMapComponent isMarkerShown />// Map with a Marker
-// <MyMapComponent
-//   isMarkerShown
-//   googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-//   loadingElement={<div style={{ height: `100%` }} />}
-//   containerElement={<div style={{ height: `400px` }} />}
-//   mapElement={<div style={{ height: `100%` }} />}
-// />;
